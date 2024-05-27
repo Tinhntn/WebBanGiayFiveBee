e937f0ccf8724571aba6bd1304d7d63a105e01e2
Tinhntn <ntinh4939@gmail.com>
2024-05-27 22:17:11 +0700

    Day code lan dau tien

---
+++ src/main/java/poly/edu/duantotnghiep/DuantotnghiepApplication.java
@@ -0,0 +1,16 @@
+package poly.edu.duantotnghiep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuantotnghiepApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        SpringApplication.run(DuantotnghiepApplication.class, args
+
+        );
+    }
+
+}
---
+++ src/test/java/poly/edu/duantotnghiep/DuantotnghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.edu.duantotnghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuantotnghiepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
